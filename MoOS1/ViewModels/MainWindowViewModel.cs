dd5ebaf37d5cc14a9d17e9dc7dcaa3a95065b9a8
blrds <blrds@yandex.ru>
2022-04-17 14:38:35 +0700

    mvvm

---
+++ MoOS1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MoOS1.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoOS1.ViewModels
+{
+    class MainWindowViewModel:ViewModel
+    {
+    }
+}
